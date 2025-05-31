50d11ebe143fee76a370da7437b3bb67e755e021
Abhishek Pradhan <pradhan.abhishek@outlook.com>
2025-05-31 15:31:16 +0530

    Added Spring boot app - base commit

---
+++ src/main/java/com/mts/pm/eq/portfolio_manager_equity_loader/PortfolioManagerEquityLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.mts.pm.eq.portfolio_manager_equity_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagerEquityLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagerEquityLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mts/pm/eq/portfolio_manager_equity_loader/PortfolioManagerEquityLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.pm.eq.portfolio_manager_equity_loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerEquityLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
